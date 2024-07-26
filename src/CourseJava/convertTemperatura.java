d21cb30aac6ea57d6d1ec54b8a61d3563467c6f8
Daniel da Silva Alves <lanhmod.dev@gmail.com>
2024-07-26 04:01:35 -0400

    Atualização da lógica do código
    Utilizei estruturas condicionais para verificar se a entrada era do tipo calculável (numérico).

--- src/CourseJava/convertTemperatura.java
+++ src/CourseJava/convertTemperatura.java
@@ -1,5 +1,5 @@
 package CourseJava;
-import java.math.BigDecimal;
+
 import java.util.Scanner;
 
 public class convertTemperatura {
@@ -9,19 +9,24 @@ public class convertTemperatura {
 		Scanner escaneador = new Scanner(System.in);
 
 		System.out.println("Me forneça a temperatura em C° Celsius: ");
-		String entrada = escaneador.nextLine();//Recebi a entrada de dados como uma String para que eu possa exibir a atual entrada de forma idêntica ao que foi inserido. 
-		BigDecimal entradaConvertida = new BigDecimal(entrada); //Aqui eu transformei a String em uma classe BigDecimal, para contar o numero de casas decimais após a vírgula com o método .scale().
-		int contaDecimais = entradaConvertida.scale();//Contando a casa com o método. 
-		String tiraVirgula = entrada.replace(",","");//Aqui estou tirando a vírgula da entrada se houver.
 		
-		Double divisor = (contaDecimais *10)/tiraVirgula;
-		double celsius = Double.parseDouble(tiraVirgula); //aqui eu fiz o famoso casting, decobrindo que para qualquer transfromação entre String e tipos primitivos é necessário utilizar métodos 
-
+		//Se a entrada for do tipo Double o programa deve fazer:
+		
+		if (escaneador.hasNextDouble()) {
+			double celsius = escaneador.nextDouble(); // Aqui estou definindo que a variável Celsius é o próximo input Double que o usuário inserir
+			double fahrenheit = (celsius * 9/ 5) + 32; //Aqui estou utilizando a variável na fórmula de conversão para fahrenheit
+			System.out.format("A conversão de %.2fC° para Fahrenheit foi: %.2f°F", celsius, fahrenheit);
+		} else if(escaneador.hasNextInt()) {
+			int celsiusInteiro = escaneador.nextInt();
+			double celsius = celsiusInteiro;
+			double fahrenheit = (celsius * 9/ 5) + 32; //Aqui estou utilizando a variável na fórmula de conversão para fahrenheit
+			System.out.format("A conversão de %.2fC° para Fahrenheit foi: %.2f°F", celsius, fahrenheit);
+		} else {
+			 System.out.println("A entrada não é um número!");
+		}
+		
 		escaneador.close();
-		
-		double fahrenheit = (celsius * 9/ 5) + 32;
-
-		System.out.format("A conversão de %sC° para Fahrenheit foi: %.2f°F",entrada, fahrenheit);
+	
 
 	}
 
